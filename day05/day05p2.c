9d2c8ae26402c4512217836813260a63d1997f9c
Albert Gustavsson <albert99gustavsson@gmail.com>
2020-12-05 06:28:59 +0100

    Solved day05

---
+++ day05/day05p2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define LOW 0
+#define HIGH 1
+
+void split_range(unsigned int *low, unsigned int *high, int half);
+unsigned int get_set_id(const char *);
+
+void split_range(unsigned int *low, unsigned int *high, int half) {
+    if(half == HIGH) {
+        *low = (*high + *low)/2 + 1;
+    } else if(half == LOW) {
+        *high = (*high + *low)/2;
+    }
+}
+unsigned int get_set_id(const char *line) {
+    unsigned int row_l = 0, row_h = 127, col_l = 0, col_h = 7;
+    for(unsigned int i = 0; i < 7; i++) {
+        if(line[i] == 'F') split_range(&row_l, &row_h, LOW);
+        else if(line[i] == 'B') split_range(&row_l, &row_h, HIGH);
+    }
+    for(unsigned int i = 7; i < 10; i++) {
+        if(line[i] == 'L') split_range(&col_l, &col_h, LOW);
+        else if(line[i] == 'R') split_range(&col_l, &col_h, HIGH);
+    }
+    return row_l*8+col_l;
+}
+
+int main(int argc, const char **argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Usage: day05p2 <input>\n");
+        return EXIT_FAILURE;
+    }
+    const char *filename = argv[1];
+    FILE *input_file = fopen(filename,"r");
+    if(input_file == NULL) {
+        perror("Couldn't open file");
+        return EXIT_FAILURE;
+    }
+    
+    int exists[128*8] = {0};
+    while(!feof(input_file)) {
+        const unsigned int max_line_length = 50;
+        char line[max_line_length];
+        fscanf(input_file, "%s\n", line);
+        unsigned int id = get_set_id(line);
+        exists[id] = 1;
+    }
+    
+    unsigned int i = 0;
+    while(!exists[i]) i++;
+    while(exists[i]) i++;
+    
+    printf("My seat ID: %u\n", i);
+
+    fclose(input_file);
+    return EXIT_SUCCESS;
+}
